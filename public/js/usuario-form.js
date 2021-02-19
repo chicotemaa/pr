9f941e3f05b09475c6294f70b2dfe9aa0c19aacd
nico michelson <nicomichelson@gmail.com>
2021-02-19 13:41:53 -0300

    select all roles in new user

--- public/js/usuario-form.js
+++ public/js/usuario-form.js
@@ -1,5 +1,38 @@
 $(document).ready(function(){
     fieldClienteUrl();
+
+    var container = document.getElementById("user_roles");
+
+
+
+    var label = document.createElement('label');
+    label.innerHTML = 'Select all:';
+    var newdiv = document.createElement('div');
+    newdiv.className = 'form-check';
+
+    var input = document.createElement("input");
+    input.type = "checkbox";
+    input.className = "form-check-input";
+    input.id = "all";
+    input.onClick="toggle(this)";
+
+    newdiv.appendChild(input);
+    newdiv.appendChild(label);
+    container.prepend(newdiv);
+
+
+    $('#all').click(function(event) {
+        if(this.checked) {
+            // Iterate each checkbox
+            $("input[name='user[roles][]']").each(function() {
+                this.checked = true;
+            });
+        } else {
+            $("input[name='user[roles][]']").each(function() {
+                this.checked = false;
+            });
+        }
+    });
 });
 
 function fieldClienteUrl () {
