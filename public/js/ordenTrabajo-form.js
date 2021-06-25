7df3a9f9de6ec9985db5005d4ae16360059b23d0
Matias Chicote <chicote.matias@gmail.com>
2021-06-25 14:04:26 -0300

    HS-2 ultima correccion formulario cambiar horario

--- public/js/ordenTrabajo-form.js
+++ public/js/ordenTrabajo-form.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     fieldUserUrl();
+    $("#ordentrabajo_horaInicio").change(function() {
+        let Imunutos=document.getElementById('ordentrabajo_horaInicio').value;
+        iniciominutos.setAttribute('value',Imunutos);
+        let iniciomin=new Date(document.getElementById('ordentrabajo_horaInicio').value);
+        let finmin=new Date(document.getElementById('ordentrabajo_horaFin').value);
+        let resultadominutos=(finmin.getTime()-iniciomin.getTime())/60000;
+        minutos.setAttribute('value',resultadominutos);
+        
+    });
+    $("#ordentrabajo_horaFin").change(function() {
+        let Imunutos=document.getElementById('ordentrabajo_horaFin').value;
+        finminutos.setAttribute('value',Imunutos);
+        let iniciomin=new Date(document.getElementById('ordentrabajo_horaInicio').value);
+        let finmin=new Date(document.getElementById('ordentrabajo_horaFin').value);
+        let resultadominutos=(finmin.getTime()-iniciomin.getTime())/60000;
+        minutos.setAttribute('value',resultadominutos);
+        
+    });
+    $("#ordentrabajo_SucursalDeCliente").change(function() {
+       console.log(SucursalDecliente.value);
+    })
 });
 
 
@@ -34,3 +55,28 @@ function fieldUserUrl () {
           },
     })
 }
+var minutos=document.getElementById('ordentrabajo_minutos');
+    var iniciominutos = document.getElementById('ordentrabajo_horaInicio')
+    var finminutos = document.getElementById('ordentrabajo_horaFin')
+    var SucursalDecliente = document.getElementById('ordentrabajo_SucursalDeCliente')
+    var idSucursalCliente=SucursalDecliente.value
+
+
+    
+    $("#edit-ordentrabajo-form").submit(function(){
+        let minutost=document.getElementById('ordentrabajo_minutos').value
+        let valorminutos=minutost;
+        let id=valorId;
+        let datos={
+            'id':parseInt(id),
+            'valor':parseInt(valorminutos)}
+        $.ajax({
+            "type": "POST" ,
+            "url": "/editar-formularioExpress/",
+            "data":datos,
+            success:function(r){
+               minutos.setAttribute('value',datos.valor);  
+            }
+            })
+        });
+        
