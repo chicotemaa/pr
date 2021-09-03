377041229115f18e50e867da74934193a9af465e
Matias Chicote <chicote.matias@gmail.com>
2021-09-03 12:32:09 -0300

    HS-32 lista de usuarios para admin/ lideres/supervisores Aplicacion

---
+++ src/Action/UserList.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Action;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Security;
+
+class UserList
+{
+    private $security;
+
+    public function __construct(EntityManagerInterface $em,Security $security)
+    {
+        $this->em = $em;
+        $this->security = $security;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $term = null ;
+        $roles = $this->security->getUser()->getRoles();
+
+        $sucursal = in_array('ROLE_SUCURSAL', $roles)? null : $this->security->getUser()->getSucursal()->getId();
+        
+        
+        $users = $this->em->getRepository(User::class)->findBySucursal($term, $sucursal);
+        
+        $results = [];
+        foreach ($users as $key => $user) {
+            $results[] = [
+                'id' => $user->getId(),
+                'text' => $user->__toString(),
+            ];
+        }
+
+        return new JsonResponse([
+            'results' => $results,
+        ]);
+    }
+}
