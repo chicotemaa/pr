5ead20e8d43e849ea5e6a7e6246e63c042eef1f9
Rodrigo Avila <rodrig1a@yahoo.com>
2021-11-24 09:11:46 -0300

    Mapa lee lat-long precargada. Añade botón buscar

--- public/js/sucursalDeCliente-form.js
+++ public/js/sucursalDeCliente-form.js
@@ -11,7 +11,7 @@ sucursalMapa.lng(); // -58.98649737984137
 */
 
 // initialize Leaflet
-var map = L.map('mapaSucursal').setView({lat: -27.41117, lon: -59.00012}, 5);
+var map = L.map('mapaSucursal');
 
 // add the OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,18 +22,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // show the scale bar on the lower left corner
 L.control.scale({imperial: false, metric: true}).addTo(map);
 
-//Inicializar marcador
+// Inicializar marcador
 let marcador = L.marker({lat: 0, lng: 0}, {draggable: false});
+// Si hay coordenadas, aplicarlas al mapa
+const BASE_ID = "sucursaldecliente_";
+const HTML_LAT = document.getElementById(BASE_ID+"Latitud").value;
+const HTML_LON = document.getElementById(BASE_ID+"Longitud").value;
+if (HTML_LAT !== "" && HTML_LON !== "") {
+  let formLat = Number.parseFloat(HTML_LAT.replace(/,/g, '.'));
+  let formLon = Number.parseFloat(HTML_LON.replace(/,/g, '.'));
+  map.setView({lat:formLat,lng:formLon}, 12);
+  marcador.setLatLng(L.latLng(formLat, formLon)).addTo(map);
+} else {
+  map.setView({lat: -27.41117, lon: -59.00012}, 5)
+}
+
 function onMapClick(e) {
     /*Interactividad, clics*/
-    const BaseId = "sucursaldecliente_";
 
-    // Moficia el mapa, dando retroalimentación
+    // Modifica el mapa, dando retroalimentación
     marcador.addTo(map);
     marcador.setLatLng({lat: e.latlng.lat, lng: e.latlng.lng});
     // Modifica el formulario
-    document.getElementById(BaseId+"Latitud").value = Number.parseFloat(e.latlng.lat).toFixed(5);
-    document.getElementById(BaseId+"Longitud").value= Number.parseFloat(e.latlng.lng).toFixed(5);
+    document.getElementById(BASE_ID+"Latitud").value = Number.parseFloat(e.latlng.lat).toFixed(5);
+    document.getElementById(BASE_ID+"Longitud").value = Number.parseFloat(e.latlng.lng).toFixed(5);
 }
 map.on('click', onMapClick);
 }
