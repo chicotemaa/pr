30d2e788b97f1caca3db6b0270338efdd7f5482b
Rodrigo Avila <rodrig1a@yahoo.com>
2021-11-24 16:32:40 -0300

    ubicar sucursal  en mapa con direccion

--- public/js/sucursalDeCliente-form.js
+++ public/js/sucursalDeCliente-form.js
@@ -31,7 +31,7 @@ const HTML_LON = document.getElementById(BASE_ID+"Longitud").value;
 if (HTML_LAT !== "" && HTML_LON !== "") {
   let formLat = Number.parseFloat(HTML_LAT.replace(/,/g, '.'));
   let formLon = Number.parseFloat(HTML_LON.replace(/,/g, '.'));
-  map.setView({lat:formLat,lng:formLon}, 12);
+  map.setView({lat:formLat,lng:formLon}, 15);
   marcador.setLatLng(L.latLng(formLat, formLon)).addTo(map);
 } else {
   map.setView({lat: -27.41117, lon: -59.00012}, 5)
@@ -48,4 +48,22 @@ function onMapClick(e) {
     document.getElementById(BASE_ID+"Longitud").value = Number.parseFloat(e.latlng.lng).toFixed(5);
 }
 map.on('click', onMapClick);
+
+/* Buscador */
+document.getElementById("buscar-btn").addEventListener("click", function() {
+  const direccion = document.getElementById("direccion").value;
+  let endpoint = new URL('/search', 'https://nominatim.openstreetmap.org');
+  let query = new URLSearchParams({'q': direccion,'format': 'jsonv2'});
+  endpoint.search = query;
+  console.log(endpoint);
+  fetch(endpoint).then(data => data.json())
+  .then(function(res) {
+    const resLat = res[0].lat;
+    const resLon = res[0].lon;
+    document.getElementById(BASE_ID+"Latitud").value = resLat;
+    document.getElementById(BASE_ID+"Longitud").value =resLon;
+    marcador.setLatLng({lat: resLat, lng: resLon}).addTo(map);
+    map.setView({lat:resLat,lng:resLon}, 15);
+  })
+})
 }
