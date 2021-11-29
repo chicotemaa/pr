8016a6db36016c917bfb3909616fb2c1cd530a06
Rodrigo Avila <rodrig1a@yahoo.com>
2021-11-29 09:46:33 -0300

    default mercure

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
