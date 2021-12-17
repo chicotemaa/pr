27a628cadf6dee839d756cda4b6c548633f8cdc3
Rodrigo Avila <rodrig1a@yahoo.com>
2021-12-17 09:14:12 -0300

    corrección de rutas de ManagerRegistry en carpeta Repository

--- src/Repository/FacilityRepository.php
+++ src/Repository/FacilityRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Facility;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Facility|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/SucursalRepository.php
+++ src/Repository/SucursalRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Sucursal;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Sucursal|null find($id, $lockMode = null, $lockVersion = null)
